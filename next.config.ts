d862a1227810dc93543ee8027a199b04d3671a51
mosihcroctiv <53885330+mosihcroctiv@users.noreply.github.com>
2026-02-17 02:51:25 +0100

    Revert "Feat/customer flow"

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            {children}
-        </>
+        <CacheProvider>
+            <ChakraProvider value={defaultSystem}>
+                {children}
+            </ChakraProvider>
+        </CacheProvider>
     );
 }
